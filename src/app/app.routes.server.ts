929d8fafc0edce91b3aed74515244c34bcc16d9b
Islam_Yasser <alex.esllam77@gmail.com>
2025-07-29 14:32:31 +0300

    u[date server rou

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'checkout/:id',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
   {
     path: 'details/:id',
